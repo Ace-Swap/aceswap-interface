abe501175d0b94d24fed8fbf20169c4146b9807c
aceswap <bliu@aeryus.com>
2021-07-05 21:38:10 -0700

    adjusted fee percent and disabled some networks

--- src/components/NetworkModal/index.tsx
+++ src/components/NetworkModal/index.tsx
@@ -139,9 +139,9 @@ export default function NetworkModal(): JSX.Element | null {
 
             <div className="flex flex-col space-y-5 overflow-y-auto">
                 {[
-                    ChainId.MAINNET,
+                    // ChainId.MAINNET,
                     // ChainId.FANTOM,
-                    ChainId.BSC,
+                    // ChainId.BSC,
                     ChainId.MATIC,
                     // ChainId.HECO,
                     // ChainId.XDAI,
